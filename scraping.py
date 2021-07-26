487f3e658a214a907370550a8d32a0243243f0d6
jilias <82242081+jilias@users.noreply.github.com>
2021-07-25 22:55:56 -0500

    Adding updates

--- scraping.py
+++ scraping.py
@@ -1,11 +1,32 @@
 #Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+
+def scrape_all():
+    #initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    
+    news_title, news_paragraph = mars_news(browser)
+    
+    # Run all scraping functions and store results in dictionary
+    data = {
+          "news_title": news_title,
+          "news_paragraph": news_paragraph,
+          "featured_image": featured_image(browser),
+          "facts": mars_facts(),
+          "last_modified": dt.datetime.now()
+    }
+    
+    #Stop webdriver and return data
+    browser.quit()
+    return data
+
+
 
 def mars_news(browser):
     #Scrape Mars News
@@ -65,8 +86,8 @@ def featured_image(browser):
 def mars_facts():
     #add try/except for error handling
     try: 
-    # pulling the table into a dataframe so that it will be readable
-    df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        # pulling the table into a dataframe so that it will be readable
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
     
     except BaseException:
         return None
@@ -77,4 +98,29 @@ def mars_facts():
     #Convert Dataframe into HTML format add bootstrap
     return df.to_html()
 
-browser.quit()
+
+def mars_hemisphere(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+
+    links = browser.find_by_css('a.product-item img')
+    title = browser.find_by_css('h3.title')
+
+    for image in range(len(links)): 
+        hemispheres = {}
+        browser.find_by_css('a.product-item img')[image].click()
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemispheres['img_url'] = sample_elem['href']
+        hemispheres['title'] = browser.find_by_css('h2.title').text
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+
+if __name__ ==  "__main__":
+    
+    
+    #if running as script print scraped data
+    print(scrape_all())
