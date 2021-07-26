a2d3dabaf105f48a9a4f353795ab7cbe5dd935bc
jilias <82242081+jilias@users.noreply.github.com>
2021-07-25 20:27:41 -0500

    updating scraping file

---
+++ scraping.py
@@ -0,0 +1,80 @@
+#Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+
+def mars_news(browser):
+    #Scrape Mars News
+    #visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+
+    #optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+    #html parser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    
+    #add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+
+        #use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+
+        #use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+        
+    except AttributeError:
+        return None, None
+    
+    return news_title, news_p
+
+def featured_image(browser):
+    # Visit url
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    #Add try/except for error handling
+    try:
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+        
+    except  AttributeError:
+        return None
+
+
+    # Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    
+    return img_url
+
+def mars_facts():
+    #add try/except for error handling
+    try: 
+    # pulling the table into a dataframe so that it will be readable
+    df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    
+    except BaseException:
+        return None
+    #Assign Columns 
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
+    
+    #Convert Dataframe into HTML format add bootstrap
+    return df.to_html()
+
+browser.quit()
